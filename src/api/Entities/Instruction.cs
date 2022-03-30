78e3f5db37c9bc36836295c839e046b47612afe9
Patric <spuute@gmail.com>
2022-03-30 16:43:04 +0200

    feat(api): adds entities

---
+++ src/api/Entities/Instruction.cs
@@ -0,0 +1,9 @@
+namespace api.Entities
+{
+    public class Instruction
+    {
+        public int Id { get; set; }
+        public int StepNumber { get; set; }
+        public string StepName { get; set; }
+    }
+}
