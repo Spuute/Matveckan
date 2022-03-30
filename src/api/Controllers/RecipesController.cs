6e9e6df03c166a05d30dca26fdaa29e9ad5b0ecc
Patric Bergkvist <70053524+Spuute@users.noreply.github.com>
2022-03-30 21:44:08 +0200

    chore(api): adds getAllRecipes method

--- src/api/Controllers/RecipesController.cs
+++ src/api/Controllers/RecipesController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
+using api.Entities;
 
 namespace api.Controllers
 {
@@ -7,6 +8,11 @@ namespace api.Controllers
     [ApiController]
     public class RecipesController : ControllerBase
     {
-        
+        [HttpGet]
+        public async Task<IActionResult> GetAllRecipes() {
+            throw new NotImplementedException();
+        }
+
+
     }
 }
