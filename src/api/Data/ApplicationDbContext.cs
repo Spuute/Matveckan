3d459d87c54265fd379776eafa107984d61afd7f
Patric <spuute@gmail.com>
2022-03-30 16:56:21 +0200

    chore(api): adds entities to dbcontext

--- src/api/Data/ApplicationDbContext.cs
+++ src/api/Data/ApplicationDbContext.cs
@@ -1,3 +1,4 @@
+using api.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace api.Data
@@ -5,5 +6,9 @@ namespace api.Data
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Recipe> Recipes { get; set; }
+        public DbSet<Ingredient> Ingredients { get; set; }
+        public DbSet<Instruction> Instructions { get; set; }
     }
 }
